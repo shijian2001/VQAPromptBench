0863a792e8875f9e16068e3197f05a3074eab2c1
shijian2001 <wsj653654736@163.com>
2024-06-08 13:27:10 +0000

    Add seedbench dataset

--- singe_imageqa_datasets.py
+++ singe_imageqa_datasets.py
@@ -2,8 +2,9 @@ from base_vqa_datasets import SingleVQADatsetInstance, BaseSingleVQADataset
 from datasets import load_from_disk
 
 single_image_qa_datasets = {
-    "blink" : ("BLINK", "/linxindisk/VQABench/subset/blink"),
-    "mmbench" : ("MMBench", "/linxindisk/VQABench/subset/mmbench"),
+    "blink" : ("BLINK", "./subset/blink"),
+    "mmbench" : ("MMBench", "./subset/mmbench"),
+    "seedbench1": ("SeedBench1", "./subset/seedbench1")
 
 }
 
@@ -62,4 +63,29 @@ class MMBench(SingleVQADatsetInstance):
 
         standard_dataset = standard_dataset.map(_create_choices)
         standard_dataset = standard_dataset.remove_columns(["A", "B", "C", "D"])
+        return standard_dataset
+
+class SeedBench1(SingleVQADatsetInstance):
+    def __init__(self, dataset_path):
+        self.dataset = load_from_disk(dataset_path)
+
+    def get_standard_dataset(self):
+
+        standard_dataset = self.dataset.select_columns(["image", "question", "answer", "choice_a", "choice_b", "choice_c", "choice_d"]).rename_column("image", "image_list")
+
+        def _process_data(sample):
+            
+            assert len(sample["image_list"]) == 1
+            sample["image"] = sample["image_list"][0]
+            sample["choices"] = [sample[option] for option in ["choice_a", "choice_b", "choice_c", "choice_d"]]
+
+            answer_to_index = {"A": 0, "B": 1, "C": 2, "D": 3}
+            index = answer_to_index[sample["answer"]]
+
+            sample["answer"] = sample["choices"][index]
+
+            return sample
+
+        standard_dataset = standard_dataset.map(_process_data)
+        standard_dataset = standard_dataset.remove_columns(["choice_a", "choice_b", "choice_c", "choice_d", "image_list"])
         return standard_dataset
