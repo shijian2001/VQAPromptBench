bfd0e8e659757ea12d6fc5deeb33137eef83d7cc
shijian2001 <wsj653654736@163.com>
2024-07-01 14:42:36 +0000

    Add vsft for phi3vision

--- finetune/vsft_phi3vision.py
+++ finetune/vsft_phi3vision.py
@@ -1,26 +1,23 @@
 import torch
 from datasets import load_dataset, load_from_disk
-from transformers import AutoProcessor, Idefics2ForConditionalGeneration
+from transformers import AutoModelForCausalLM, AutoProcessor
 from transformers import TrainingArguments, Trainer
+
+Phi3VisionTemplate = "{% for message in messages %}{{ '<|' + message['role'] + '|>' + '\n' }}{% for line in message['content'] %}{% if line['type'] == 'text' %}{{ line['text'] }}{% elif line['type'] == 'image' %}{{ '<|image_1|>' }}{% endif %}{% endfor %}<|end|>\n{% endfor %}{% if add_generation_prompt and messages[-1]['role'] != 'assistant' %}{{- '\n' -}}{% endif %}"
 
 class DataCollator:
     def __init__(self, processor):
         self.processor = processor
-        # self.image_token_id = processor.tokenizer.additional_special_tokens_ids[
-        #     processor.tokenizer.additional_special_tokens.index("<image>")
-        # ]
 
     def __call__(self, examples):
-        texts = []
-        images = []
-        for example in examples:
-            image = example["images"]
-            messages = example["messages"]
-            text = processor.apply_chat_template(messages, add_generation_prompt=False)
-            texts.append(text.strip())
-            images.append([image])
+        assert len(examples) == 1, 'Phi-3-V only supports batch_size == 1'
+        example = examples[0]
 
-        batch = processor(text=texts, images=images, return_tensors="pt", padding=True)
+        image = example["images"]
+        messages = example["messages"]
+        text = processor.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=False)
+
+        batch = processor(text=text, images=image, return_tensors="pt", padding=True)
 
         labels = batch["input_ids"].clone()
         labels[labels == processor.tokenizer.pad_token_id] = -100
@@ -29,15 +26,17 @@ class DataCollator:
         return batch
 
 processor = AutoProcessor.from_pretrained(
-    "HuggingFaceM4/idefics2-8b",
-    do_image_splitting=False
+    "microsoft/Phi-3-vision-128k-instruct",
+    trust_remote_code=True
 )
+processor.tokenizer.chat_template = Phi3VisionTemplate
 
 data_collator = DataCollator(processor)
 
-model = Idefics2ForConditionalGeneration.from_pretrained(
-    "HuggingFaceM4/idefics2-8b",
-    torch_dtype=torch.float16,
+model = AutoModelForCausalLM.from_pretrained(
+    "microsoft/Phi-3-vision-128k-instruct",
+    trust_remote_code=True,
+    torch_dtype=torch.bfloat16,
     _attn_implementation="flash_attention_2", # Only available on A100 or H100
     device_map="auto"
 )
@@ -52,22 +51,22 @@ train_dataset, eval_dataset = dataset["train"], dataset["test"]
 
 training_args = TrainingArguments(
     num_train_epochs=1,
-    per_device_train_batch_size=16, # 8 A100
-    per_device_eval_batch_size=4, # 8 A 100
+    per_device_train_batch_size=1, # 8 A100
+    per_device_eval_batch_size=1, # 8 A 100
     gradient_accumulation_steps=1, # modified along with above
     warmup_ratio=0.03,
     lr_scheduler_type="cosine",
     learning_rate=2e-4,
     weight_decay=0.,
     logging_steps=1,
-    output_dir="../logs/checkpoints/idefics2-8b-multi-templates-vsft", # should be modifed
+    output_dir="../logs/checkpoints/phi-3-vision-multi-templates-vsft", # should be modifed
     save_strategy="steps",
     save_steps=50000,
     save_total_limit=1,
     eval_strategy="steps",
     eval_steps=250,
     fp16=True,
-    hub_model_id="shijianS01/idefics2-8b-multi-templates-vsft",
+    hub_model_id="shijianS01/phi-3-vision-multi-templates-vsft",
     remove_unused_columns=False,
     report_to="wandb", # wandb or none
 )
