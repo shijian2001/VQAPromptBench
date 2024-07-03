035ac8b4f32af76ee086f1103cf0b432b927cf58
shijian2001 <wsj653654736@163.com>
2024-07-03 08:23:34 +0000

    Update VQAPromptBayesOptimizer; use optuna; support embedding; add logging

--- prompt_optimization/bayesian_op.py
+++ prompt_optimization/bayesian_op.py
@@ -1,18 +1,51 @@
 import numpy as np
 from tqdm import tqdm
 from typing import *
-from skopt import gp_minimize
-from skopt.space import Categorical
+import optuna
+import logging
+import warnings
+
+warnings.filterwarnings("ignore")
+
+logger = logging.getLogger("optuna")
+logger.setLevel(logging.CRITICAL)
+
 
 class VQAPromptBayesOptimizer:
-    def __init__(self, vqa_model, vqa_dataset, prompts_pool, n_calls=10, random_state=42):
-        self.vqa_model = vqa_model
-        self.vqa_dataset = vqa_dataset
-        self.prompts_pool = prompts_pool
-        self.n_calls = n_calls
-        self.random_state = random_state
+    def __init__(
+            self, 
+            vqa_model, 
+            vqa_dataset, 
+            prompts_pool: List[str], 
+            n_trials: int=10, 
+            random_state: int=42, 
+            num_samples: Union[None, int]=None, 
+            enable_option_ordering: bool=False
+        ):
 
-        self.search_space = [Categorical(prompts_pool, name='PromptTemplate')]
+            self.vqa_model = vqa_model
+            self.vqa_dataset = vqa_dataset
+            self.prompts_pool = prompts_pool
+            self.n_trials = n_trials
+            self.random_state = random_state
+            self.num_samples = num_samples
+            self.enable_option_ordering = enable_option_ordering
+            self.study = optuna.create_study(direction='maximize', sampler=optuna.samplers.TPESampler(seed=self.random_state))
+
+            self._construct_loopup_table()
+
+    def _construct_loopup_table(self):
+        from sentence_transformers import SentenceTransformer
+
+        # Get embeddings
+        print(f"Embedding {len(self.prompts_pool)} prompt templates.....")
+        sentence_transformer = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+        prompt_embeddings = sentence_transformer.encode(self.prompts_pool)
+        print("Finish embedding")
+
+        # Construct lookup table
+        self.prompt_embedding_tuple_list = [tuple(embedding) for embedding in prompt_embeddings]
+        self.prompt_emb_lookup_table = {key: value for key, value in zip(self.prompt_embedding_tuple_list, self.prompts_pool)}
 
     def _build_prompt_func(self, prompt_template: str):
         def imageqa_prompt(question: str, context: str, choices: List[str]):
@@ -26,29 +59,52 @@ class VQAPromptBayesOptimizer:
 
     def evaluator(self, prompt_template):
         accs = []
-        for sample in tqdm(self.vqa_dataset):
-            result = self.vqa_model.multiple_choice_qa_random_ordering(
-                data = sample["image"],
-                question = sample["question"],
-                context=sample["context"],
-                choices = sample["choices"],
-                answer = sample["answer"],
-                prompt_func= self._build_prompt_func(prompt_template)
-            )
+
+        if self.num_samples is not None:
+            dataset = self.vqa_dataset.shuffle().select(range(self.num_samples))
+        else:
+            dataset = self.vqa_dataset
+
+        for sample in tqdm(dataset):
+
+            if self.enable_option_ordering:
+                result = self.vqa_model.multiple_choice_qa_random_ordering(
+                    data=sample["image"],
+                    question=sample["question"],
+                    context=sample["context"],
+                    choices=sample["choices"],
+                    answer=sample["answer"],
+                    prompt_func=self._build_prompt_func(prompt_template)
+                )
+            else:
+                result = self.vqa_model.multiple_choice_qa(
+                    data=sample["image"],
+                    question=sample["question"],
+                    context=sample["context"],
+                    choices=sample["choices"],
+                    answer=sample["answer"],
+                    prompt_func=self._build_prompt_func(prompt_template)
+                )
+
             accuracy = result["accuracy"]
             accs.append(accuracy)
+
         metric = np.mean(accs)
         print("Acc:", metric)
+
         return metric
 
-    def objective(self, params):
-        prompt_template = params[0]
+    def objective(self, trial):
+        prompt_emb = trial.suggest_categorical('PromptEmbedding', self.prompt_embedding_tuple_list)
+        prompt_template = self.prompt_emb_lookup_table[prompt_emb]
         prompt_index = self.prompts_pool.index(prompt_template) + 1
-        print(f"Evaluating prompt_{prompt_index}")
-        return -self.evaluator(prompt_template)
+        print(f"Evaluating prompt_{prompt_index}:\n##############\n{prompt_template}\n##############")
+        return self.evaluator(prompt_template)
 
     def optimize(self):
-        res = gp_minimize(self.objective, self.search_space, n_calls=self.n_calls, random_state=self.random_state)
-        best_prompt = res.x[0]
-        best_performance = -res.fun
+        self.study.optimize(self.objective, n_trials=self.n_trials)
+        best_trial = self.study.best_trial
+        best_prompt_emb = best_trial.params['PromptEmbedding']
+        best_prompt = self.prompt_emb_lookup_table[best_prompt_emb]
+        best_performance = best_trial.value
         return best_prompt, best_performance
--- prompt_search.py
+++ prompt_search.py
@@ -4,18 +4,18 @@ from prompt_factory import detailed_imageqa_prompt
 from prompt_optimization import VQAPromptBayesOptimizer
 import json
 
+# prompts search space
+prompts_pool = json.load(open("prompt_factory/prompt_library.json", "r"))["MultiChoiceImageQa"]
+
 # load vqa model
 vqa_model = ImageQAModel("deepseek-vl-7b-chat", prompt_func=detailed_imageqa_prompt, enable_choice_search=True, torch_device=1)
 
 # load vqa dataset
 vqa_dataset = SingleImageQADataset("mmbench").get_dataset()
 
-# prompts search space
-prompts_pool = json.load(open("prompt_factory/prompt_library.json", "r"))["MultiChoiceImageQa"]
-
 # define bayes searcher
-optimizer = VQAPromptBayesOptimizer(vqa_model, vqa_dataset, prompts_pool, n_calls=10, random_state=34)
+optimizer = VQAPromptBayesOptimizer(vqa_model, vqa_dataset, prompts_pool, n_trials=10, random_state=42)
 best_prompt, best_performance = optimizer.optimize()
 best_prompt_index = prompts_pool.index(best_prompt) + 1
-print(f"Best prompt is prompt_{best_prompt_index}:\n{best_prompt}")
+print(f"Best prompt is prompt_{best_prompt_index}:\n##############\n{best_prompt}\n##############")
 print(f"Best Acc: {best_performance:.4f}")
