d04349f6855642664be409203291e46141a34ef4
shijian2001 <wsj653654736@163.com>
2024-09-21 18:38:10 +0000

    Update template generator utils

--- prompt_factory/template_utils.py
+++ prompt_factory/template_utils.py
@@ -3,43 +3,68 @@
 from .template_generator import *
 from .vqa_meta_data import *
 from typing import *
+from tqdm import tqdm
 
-def get_random_question_template(enable_balanced_pattern: bool=True):
-    """Generate a random prompt template with diverse attributes."""
-    return TemplateGenerator(QUESTION_PATTERNS, enable_balanced_pattern=enable_balanced_pattern).generate()
+class QuestionTemplateGenerator:
+    def __init__(self, enable_balanced_pattern: bool=True):
+        self.generator = TemplateGenerator(QUESTION_PATTERNS, enable_balanced_pattern=enable_balanced_pattern)
 
-def get_random_vqa_template(enable_context: bool=False, enable_shuffle: bool=False, enable_balanced_pattern: bool=True):
-    """Generate a random vqa template with diverse attributes."""
+    def generate(self):
+        return self.generator.generate()
+    
+    @property
+    def num_all_potential_prompts(self):
+        return self.generator.num_all_potential_prompts
 
-    choices_template_generator = TemplateGenerator(CHOICES_PATTERNS, enable_balanced_pattern=enable_balanced_pattern)
-    question_template = get_random_question_template()
-    choices_template = choices_template_generator.generate()
-    templates = [question_template, choices_template]
+class VQATemplateGenerator:
+    def __init__(self, enable_context: bool=False, enable_shuffle: bool=False, enable_balanced_pattern: bool=True):
+        self.enable_context = enable_context
+        self.enable_shuffle = enable_shuffle
+        self.question_generator = QuestionTemplateGenerator(enable_balanced_pattern)
+        self.choices_template_generator = TemplateGenerator(CHOICES_PATTERNS, enable_balanced_pattern=enable_balanced_pattern)
+        if enable_context:
+            self.context_template_generator = TemplateGenerator(CONTEXT_PATTERNS, enable_balanced_pattern=enable_balanced_pattern)
 
-    if enable_context:
-        context_template_generator = TemplateGenerator(CONTEXT_PATTERNS, enable_balanced_pattern=enable_balanced_pattern)
-        context_template = context_template_generator.generate()
-        templates = [question_template, context_template, choices_template]
+    def generate(self):
+        question_template = self.question_generator.generate()
+        choices_template = self.choices_template_generator.generate()
+        
+        templates = [question_template, choices_template]
+        
+        if self.enable_context:
+            context_template = self.context_template_generator.generate()
+            templates.insert(1, context_template)  # insert context between question and choices
 
-    # Consider different relative element poisitions
-    if enable_shuffle:
-        random.shuffle(templates)
+        if self.enable_shuffle:
+            random.shuffle(templates)
 
-    vqa_prompt_template = '\n'.join(templates)
-    return vqa_prompt_template
+        return '\n'.join(templates)
+    
+    @property
+    def num_all_potential_prompts(self):
+        total = self.question_generator.num_all_potential_prompts * self.choices_template_generator.num_all_potential_prompts
+        if self.enable_context:
+            total *= self.context_template_generator.num_all_potential_prompts
+        return total
 
-def generate_templates_set(template_generator: Callable, num_templates: int):
+def generate_templates_set(template_generator, num_templates: int):
     """Generate a specified number of prompts with no duplicate elements"""
+    assert num_templates <= template_generator().num_all_potential_prompts, (
+        "The number of generated templates should be less than or equal to the capacity of the template generator."
+    )
     templates_set = set()
-    while len(templates_set) < num_templates:
-        template = template_generator()
-        templates_set.add(template)
+    with tqdm(total=num_templates, desc="Generating templates") as pbar:
+        while len(templates_set) < num_templates:
+            template = template_generator().generate()
+            if template not in templates_set:
+                templates_set.add(template)
+                pbar.update(1)
     return list(templates_set)
 
 def assign_templates(num_data: int, templates_set: List[str]) -> List[str]:
     """Assign a fixed set of prompt templates to the data and ensure that each template is sampled."""
     assert num_data >= len(templates_set), (
-        "The number of items should be greater than or equal to the number of templates."
+        "The number of data should be greater than or equal to the number of templates."
     )
     randomized_templates = random.sample(templates_set, len(templates_set))
     all_templates = randomized_templates + random.choices(templates_set, k=num_data - len(templates_set))
